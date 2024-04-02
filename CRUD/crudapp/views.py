84534f902179092f01a11235c07ebf0cfc90fc2e
mChoczay <msgfromgod3@gmail.com>
2024-04-02 12:51:37 +0200

    login implementation

--- CRUD/crudapp/views.py
+++ CRUD/crudapp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib import auth
 from .forms import CreateUserForm, LoginForm
+
 
 
 def home(request):
     return render(request, 'crudapp/index.html')
 
-# Register view
+# - Register user
 
 def register(request):
     form = CreateUserForm()
@@ -14,6 +17,24 @@ def register(request):
         if form.is_valid():
             form.save()
             # return redirect('login')
-    context = {'form': form}
+    context = {'register_form': form}
     return render(request, 'crudapp/register.html', context=context)
 
+
+# - Login user
+
+def login(request):
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                return redirect('')
+    context = {'login_form': form}
+    return render(request, 'crudapp/login.html', context=context)
