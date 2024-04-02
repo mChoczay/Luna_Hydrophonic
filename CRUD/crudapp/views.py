c907f306b8a1fce6d9fbf3d118a0ae93bbc586b5
mChoczay <msgfromgod3@gmail.com>
2024-04-02 12:24:36 +0200

    register

--- CRUD/crudapp/views.py
+++ CRUD/crudapp/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, LoginForm
 
 
 def home(request):
     return render(request, 'crudapp/index.html')
+
+# Register view
+
+def register(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # return redirect('login')
+    context = {'form': form}
+    return render(request, 'crudapp/register.html', context=context)
 
