6dd4b3eef23d69ac651ee3fcfb26577b6a480e9a
mChoczay <msgfromgod3@gmail.com>
2024-04-02 12:55:50 +0200

    logout and some code formatting

--- CRUD/crudapp/views.py
+++ CRUD/crudapp/views.py
@@ -4,37 +4,49 @@ from django.contrib import auth
 from .forms import CreateUserForm, LoginForm
 
 
-
 def home(request):
-    return render(request, 'crudapp/index.html')
+    return render(request, "crudapp/index.html")
+
 
 # - Register user
 
+
 def register(request):
     form = CreateUserForm()
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = CreateUserForm(request.POST)
+
         if form.is_valid():
             form.save()
-            # return redirect('login')
-    context = {'register_form': form}
-    return render(request, 'crudapp/register.html', context=context)
+            return redirect("login")
+
+    context = {"register_form": form}
+    return render(request, "crudapp/register.html", context=context)
 
 
 # - Login user
 
+
 def login(request):
     form = LoginForm()
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = LoginForm(request, data=request.POST)
+
         if form.is_valid():
-            username = request.POST.get('username')
-            password = request.POST.get('password')
+            username = request.POST.get("username")
+            password = request.POST.get("password")
 
             user = authenticate(request, username=username, password=password)
 
             if user is not None:
                 auth.login(request, user)
-                return redirect('')
-    context = {'login_form': form}
-    return render(request, 'crudapp/login.html', context=context)
+                return redirect("")
+
+    context = {"login_form": form}
+    return render(request, "crudapp/login.html", context=context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect("")
